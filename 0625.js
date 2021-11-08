2d77a0ea66a0d0b12f9f728d90162200c4ee157b
megannvarciee <60273605+megannvarciee@users.noreply.github.com>
2021-11-07 21:22:32 -0500

    Update 0625.js

--- 0625.js
+++ 0625.js
@@ -1,61 +1,66 @@
 function myFunction(insertedDate, insertedStation) {
+  const api_url =
+    "https://mesonet.agron.iastate.edu/api/1/obhistory.json?network=MI_ASOS&station=" +
+    insertedStation.toString() +
+    "&date=" +
+    insertedDate.toString() +
+    "&full=true";
 
-const api_url = 
-      "https://mesonet.agron.iastate.edu/api/1/obhistory.json?network=MI_ASOS&station=" + insertedStation.toString() + "&date=" + insertedDate.toString() + "&full=true";
-  
-// Defining async function
-async function getapi(url) {
-    
+  console.log(api_url);
+
+  // Defining async function
+  async function getapi(url) {
     // Storing response
     const response = await fetch(url);
-    
+
     // Storing data in form of JSON
     var data = await response.json();
-    
+
     let temp = [];
-    let time = []; 
-    let dewp = []; 
+    let time = [];
+    let dewp = [];
+    let wnd = [];
+    let gst = [];
 
-	for (let i = 0; i < data['data'].length; i++) {
-		if (data['data'][i]['tmpf'] != null) {
-  				temp.push(parseInt(data['data'][i]['tmpf']) + 'ºF');
-  				time.push(data['data'][i]['local_valid']);
-  				dewp.push(parseInt(data['data'][i]['dwpf']) + 'ºF');
+    for (let i = 0; i < data["data"].length; i++) {
+      if (data["data"][i]["tmpf"] != null) {
+        temp.push(parseInt(data["data"][i]["tmpf"]));
+        time.push(data["data"][i]["local_valid"]);
+        dewp.push(parseInt(data["data"][i]["dwpf"]));
+        wnd.push(parseInt(data["data"][i]["sknt"]));
+        if (data["data"][i]["gust"] != null) {
+          gst.push(parseInt(data["data"][i]["gust"]));
+        } else {
+          gst.push(" ");
+        }
+      }
     }
+
+    var header = [
+      "Time (ET)",
+      "Temp (ºF)",
+      "Dewpoint (ºF)",
+      "Wind (kts)",
+      "Wind Gusts (kts)"
+    ];
+    var myTable = "<table><tr>";
+
+    for (let i = 0; i < header.length; i++) {
+      myTable += "<th>" + header[i] + "</th>";
+    }
+
+    for (let i = 0; i < temp.length; i++) {
+      myTable += "</tr><tr><td>" + time[i] + "</td>";
+      myTable += "<td>" + temp[i] + "</td>";
+      myTable += "<td>" + dewp[i] + "</td>";
+      myTable += "<td>" + wnd[i] + "</td>";
+      myTable += "<td>" + gst[i] + "</td>";
+    }
+
+    myTable += "</tr></table>";
+    document.getElementById("myDiv").innerHTML = myTable;
   }
 
-var data = [{
-  type: 'table',
-  
-  header: {
-    values: [["<b>TIME (ET)</b>"], ["<b>TEMP</b>"],
-				 ["<b>DEWP</b>"]],
-    align: "center",
-    line: {width: 1, color: 'black'},
-    fill: {color: "grey"},
-    font: {family: "Arial", size: 12, color: "white"}
-  },
-  cells: {
-    values: [[time][0], [temp][0], [dewp][0]],
-    align: "center",
-    line: {color: "black", width: 1},
-    font: {family: "Arial", size: 18, color: ["black"]}, 
-    width: 100, 
-    height: 32, 
-    fill: {color: "none"}
-  }
-}]
-
-var layout = {
-  autosize: false,
-  width: 800,
-  height: 800};
-
-Plotly.newPlot('myDiv', data, layout);
-
-  
-    	}
-    	getapi(api_url)
-
+  getapi(api_url);
 }
 
